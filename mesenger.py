cbbc3c17141d5b1fd5978bba07d2ef7092ba6a89
KirillKiyko <k.kiyko@gargoylze.com>
2018-01-22 17:40:55 +0200

    Add sender id

--- mesenger.py
+++ mesenger.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, request
 from pymessenger.bot import Bot
 from answers import get_answer
@@ -27,14 +28,13 @@ def receive_message():
                     # Facebook Messenger ID for user so we know where to send response back to
                     recipient_id = message['sender']['id']
                     if message['message'].get('text'):
-                        print(type(message['message']))
-                        print(message['message'])
+                        print(message['message'].get('text'), message['sender'].get('id'))
                         print message
-                        response_sent_text = get_answer('sender', message['message'].get('text'))
+                        response_sent_text = get_answer(message['sender'].get('id'), message['message'].get('text'))
                         send_message(recipient_id, response_sent_text)
                     # if user sends us a GIF, photo,video, or any other non-text item
                     if message['message'].get('attachments'):
-                        response_sent_nontext = get_answer('sender', message['message'].get('text'))
+                        response_sent_nontext = get_answer(message['sender'].get('id'), message['message'].get('text'))
                         send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
 
