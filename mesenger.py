4c84d4cfce3f77fdebd91e3b6e65a82996f27f59
KirillKiyko <k.kiyko@gargoylze.com>
2018-01-22 17:10:23 +0200

    check request from user

--- mesenger.py
+++ mesenger.py
@@ -29,11 +29,11 @@ def receive_message():
                     if message['message'].get('text'):
                         print(type(message['message']))
                         print(message['message'].get('text'))
-                        response_sent_text = get_answer(message['message'].get('text'))
+                        response_sent_text = get_answer('sender', message['message'].get('text'))
                         send_message(recipient_id, response_sent_text)
                     # if user sends us a GIF, photo,video, or any other non-text item
                     if message['message'].get('attachments'):
-                        response_sent_nontext = get_answer(message['message'].get('text'))
+                        response_sent_nontext = get_answer('sender', message['message'].get('text'))
                         send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
 
